2bb6c2589f9ee260657205d1c70267adff55f096
alisiak03 <145369992+alisiak03@users.noreply.github.com>
2023-10-04 11:57:27 +0100

    add and multiply

---
+++ Ct.java
@@ -0,0 +1,13 @@
+public class Ct {
+	// add and mulptily method 
+	public static int add(int num1, int num2) {
+		
+       result = num1 + num2;
+       System.out.print(result);
+	}
+    public static int multiply(int num1, int num2) {
+    	result = num1 * num2;
+    	System.out.print(result);
+    }
+
+}
